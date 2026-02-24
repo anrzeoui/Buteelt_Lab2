9c3cc42b246cfd7d31ea8db065e360d8e2043d01
anrzeoui <anar.urnukhbayar@gmail.com>
2026-02-24 22:41:12 +0800

    test action

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Shape rectangle = new Rectangle(2, 3);
         Shape circle = new Circle(5);
         Shape square = new Square(5);
-        
+        // Action check
         Renderer rectangleRenderer = new Renderer(rectangle);
         Renderer circleRenderer = new Renderer(circle);
         Renderer squareRenderer = new Renderer(square);
